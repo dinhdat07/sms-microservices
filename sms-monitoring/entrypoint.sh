c8edcabebda093022afd7c611823b1b3fdb02bb4
dinhdat07 <dinhdatnguyen0710@gmail.com>
2026-07-16 17:43:17 +0700

    feat(monitoring): refactor config parser and entrypoint for swarm overlay network

--- sms-monitoring/entrypoint.sh
+++ sms-monitoring/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Route the virtual subnet to the simulator container
-SIMULATOR_IP=$(getent hosts sms_simulator | awk '{ print $1 }')
+SIMULATOR_IP=$(getent hosts tasks.simulator | awk '{ print $1 }' | head -n 1)
+if [ -z "$SIMULATOR_IP" ]; then
+    SIMULATOR_IP=$(getent hosts simulator | awk '{ print $1 }' | head -n 1)
+fi
+
 if [ -n "$SIMULATOR_IP" ]; then
     echo "Adding route for 10.1.0.0/16 via $SIMULATOR_IP"
     ip route add 10.1.0.0/16 via $SIMULATOR_IP
